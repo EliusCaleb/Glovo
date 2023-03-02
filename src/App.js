5445e576a86d4ae5a738fb2d63351284424b60df
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-03-02 12:11:29 +0300

    create the card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Cards from './components/Cards';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       <Home/>
+       <Cards/>
     </div>
   );
 }
